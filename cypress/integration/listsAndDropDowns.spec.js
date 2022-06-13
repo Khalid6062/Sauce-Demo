c1103de1ba54afb881d719bd9d18a77f26645eb5
Khalid6062 <105902160+Khalid6062@users.noreply.github.com>
2022-06-13 15:27:23 +0530

    Delete cypress/integration directory

--- cypress/integration/listsAndDropDowns.spec.js
+++
@@ -1,9 +0,0 @@
-describe('verify the list and dropdown',()=>{
-
-    it('lists and drop down',()=>{
-        cy.visit('/')
-
-        cy.get('nav nb-select').click()
-        cy.get('.options-list').contains('Dark').click()
-    })
-})
